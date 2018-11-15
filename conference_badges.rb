ba0ccfe7a8259bae3f4f14f00a1147a557958d89
lumrachele <rlum894@gmail.com>
2018-11-15 15:52:45 -0500

    conference badges and assign rooms

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,35 @@
-# Write your code here.
+def badge_maker (name)
+  "Hello, my name is #{name}."
+end
+
+
+def batch_badge_creator (attendees)
+  badges = []
+  attendees.each do |name|
+    badges.push(badge_maker(name))
+  end
+  badges
+end
+
+
+def assign_rooms (attendees)
+  rooms = []
+  x = 0
+  attendees.each do |name|
+    x += 1
+    rooms.push("Hello, #{name}! You'll be assigned to room #{x}!")
+  end
+  rooms
+end
+
+
+def printer (attendees)
+  x = 0
+  while x < 7
+    attendees.each do |name|
+      puts batch_badge_creator(attendees)[x]
+      puts assign_rooms(attendees)[x]
+      x += 1
+    end
+  end
+end
